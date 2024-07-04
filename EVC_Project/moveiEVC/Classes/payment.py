2c66be53f49c1a13ee4d88ab0e3d9cff56a5ddea
Moawia <msalwafi1@gmail.com>
2024-07-04 08:59:28 +0300

    sci-fi posters

--- EVC_Project/moveiEVC/Classes/payment.py
+++ EVC_Project/moveiEVC/Classes/payment.py
@@ -1,18 +1,8 @@
 import random
-
-class User:
-    def __init__(self, name, number):
-        self.name = name
-        self.number = number
-
-    def get_user_name(self):
-        return self.name
-
-    def get_user_number(self):
-        return self.number
+from user import User
 
 class Payment:
-    def __init__(self, card_number, name, ex_date, cvc_num, user):
+    def _init_(self, card_number, name, ex_date, cvc_num, user):
         self.card_number = card_number
         self.name = name
         self.ex_date = ex_date
@@ -36,31 +26,16 @@ class Payment:
         return True
 
     def generate_reservation_number(self):
-        # Generate a random reservation number
+
+        # Generate reservation number
         return random.randint(10000, 99999)
 
     def send_sms(self):
-        
+
         # Generate reservation number
         reservation_number = self.generate_reservation_number()
 
+       
         # Send SMS
-        message_body = f"Hello {self.user.get_user_name()}, your reservation (#{reservation_number}) has been confirmed. Enjoy your movie🎬⭐!"
+        message_body = f"Hello {self.user.get_user_name()}, your reservation (#{reservation_number}) has been confirmed. Enjoy your movie!"
         print(message_body)
-      
-        
-# main
-if __name__ == "__main__":
-    # Create a user
-    user = User("John Doe", "+966544079083")
-
-    # Make a payment (example data)
-    payment1 = Payment("1234567812345678", "John Doe", "12/23", "123", user)
-
-    # Check payment details
-    if payment1.check():
-      
-        print("Payment successful and reservation confirmed.")
-        payment1.send_sms()
-    else:
-        print("Invalid payment details.")
